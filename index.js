1f58e9cce0c6f4e9d555c1b5f08fbd8b30b6d1f1
devmaniac1 <mohammed.20221656@iit.ac.lk>
2023-12-23 11:50:33 +0530

    tablets-all

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const buttons = document.querySelectorAll(".icon-mobile-nav");
 const header = document.querySelector(".header");
 
-const handleClick = () => {
-  header.classList.contains("nav-open")
-    ? header.classList.remove("nav-open")
-    : header.classList.add("nav-open");
-};
+const handleClick = () => header.classList.toggle("nav-open");
 
 buttons.forEach((button) => button.addEventListener("click", handleClick));
